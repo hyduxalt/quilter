cfd4614198e87498dd868e0cec8c5bfe12c81e5f
hyduxalt <85044595+hyduxalt@users.noreply.github.com>
2024-08-13 21:18:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -499,3 +499,250 @@ function loadColorsDiv(colors_o) {
     pickerPosition++;
   }
 }
+function newPicker(color, id, order) {
+  let pickerDiv = createDiv();
+  pickerDiv.parent(colorsDiv);
+  pickerDiv.class("color-picker-container");
+  pickerDiv.style("order", order);
+
+  let picker = createColorPicker(color);
+  picker.class("color-picker");
+  picker.id(id);
+  picker.parent(pickerDiv);
+  picker.input(updateColor);
+  picker.elt.addEventListener("change", logColorChange, false);
+  picker.doubleClicked(setCurrentColor);
+  picker.attribute("title", "color");
+
+  let deleteButton = createButton("×");
+  deleteButton.parent(pickerDiv);
+  deleteButton.mousePressed(() => deleteColor(id));
+  deleteButton.attribute("title", "delete color");
+}
+
+function loadCurrentColorPicker() {
+  if (currentColorPicker) currentColorPicker.remove();
+  currentColorPicker = createColorPicker(colorScheme[currentColor]);
+  currentColorPicker.parent(select(".current-color-container"));
+  currentColorPicker.class(currentColor);
+  currentColorPicker.id("current-color-picker");
+  currentColorPicker.attribute("title", "current active color");
+  currentColorPicker.elt.addEventListener("change", currentColorChange, false);
+}
+
+function deleteColor(id) {
+  let previousColorString = colorString(colorScheme[id]);
+
+  delete colorScheme[id];
+  let thisPicker = select("#" + id).parent();
+  let order = new p5.Element(thisPicker).style("order");
+  thisPicker.remove();
+
+  let previousPattern = deepCopy(pattern_array);
+  for (var i in pattern_array) {
+    for (var j in pattern_array) {
+      pattern_array[i][j] = pattern_array[i][j].replace(id, "_");
+    }
+  }
+  let currentPattern = deepCopy(pattern_array);
+
+  logAction(
+    function () {
+      newPicker(colorParse(previousColorString), id, order);
+      colorScheme[id] = colorParse(previousColorString);
+
+      setPattern(previousPattern);
+      currentColors = deepCopy(colorScheme);
+    },
+    function () {
+      delete colorScheme[id];
+      select("#" + id)
+        .parent()
+        .remove();
+
+      setPattern(currentPattern);
+      currentColors = deepCopy(colorScheme);
+    }
+  );
+  currentColors = deepCopy(colorScheme);
+}
+
+function newColor(color) {
+  let colors = Object.keys(colorScheme);
+  for (var i in colors) {
+    if (colorScheme[colors[i]].toString() === color.toString()) {
+      return colors[i];
+    }
+  }
+
+  let id = newColorId();
+  newPicker(color, id, pickerPosition);
+  let order = pickerPosition;
+  pickerPosition++;
+
+  colorScheme[id] = color;
+
+  // history system
+  logAction(
+    function () {
+      delete colorScheme[id];
+      select("#" + id)
+        .parent()
+        .remove();
+      currentColors = deepCopy(colorScheme);
+    },
+    function () {
+      newPicker(color, id, order);
+      colorScheme[id] = color;
+      currentColors = deepCopy(colorScheme);
+    }
+  );
+  currentColors = deepCopy(colorScheme);
+
+  currentColorPicker.elt.removeEventListener("change", currentColorChange);
+  let tempCallback = function () {
+    currentColorPicker.elt.removeEventListener("change", tempCallback);
+    currentColorPicker.elt.addEventListener("change", currentColorChange);
+  };
+  currentColorPicker.elt.addEventListener("change", tempCallback, false);
+
+  currentColor = id;
+  currentColorPicker.class(id);
+  return id;
+}
+
+function newColorId() {
+  let colors = Object.keys(colorScheme);
+
+  for (var i in alphabet) {
+    if (!colors.includes(alphabet[i]) && alphabet[i] != currentColor)
+      return alphabet[i];
+  }
+  return "-";
+}
+
+function updateColor() {
+  colorScheme[this.id()] = this.color();
+
+  if (currentColorPicker.class() == this.id()) {
+    loadCurrentColorPicker();
+  }
+}
+
+function currentColorChange() {
+  currentColor = "!";
+  currentColorPicker.class(currentColor);
+}
+
+function setCurrentColor() {
+  currentColor = this.id();
+
+  loadCurrentColorPicker();
+}
+
+function checkForNewColor() {
+  if (
+    !colorScheme[currentColor] ||
+    currentColorPicker.color().toString() !==
+      colorScheme[currentColor].toString()
+  ) {
+    currentColor = newColor(currentColorPicker.color());
+  }
+}
+
+
+// Layout System Functions -----------------------------------------
+
+function loadLayout() {
+  if (layoutDiv) {
+    layoutDiv.remove();
+    layoutDiv = createSpan();
+    layoutDiv.class("layout-container");
+    layoutDiv.parent(select(".display-two"));
+  } else {
+    layoutDiv = select(".layout-container");
+  }
+
+  let transforms = Object.keys(LayoutTransforms);
+
+  for (var i = 0; i < layout_array.length; i++) {
+    let row = createDiv();
+    row.parent(layoutDiv);
+    row.class("layout-row");
+
+    for (var j = 0; j < layout_array[0].length; j++) {
+      let button = createButton(
+        loadFile("Icons/" + transforms[layout_array[i][j]] + ".svg")
+      );
+      button.parent(row);
+      button.class("layout-button");
+
+      if (i == 0 && j == 0) {
+        button.id("template");
+        button.attribute("title", "template");
+      } else {
+        let n = i;
+        let m = j;
+        button.mousePressed(() => next(n, m));
+      }
+
+      switch (layout_array[i][j]) {
+        case 0:
+          if (i != 0 || j != 0) button.attribute("title", "none");
+          break;
+        case 1:
+          button.attribute("title", "rotate 90°");
+          break;
+        case 2:
+          button.attribute("title", "rotate 180°");
+          break;
+        case 3:
+          button.attribute("title", "rotate 270°");
+          break;
+        case 4:
+          button.attribute("title", "flip horizontal");
+          break;
+        case 5:
+          button.attribute("title", "flip vertical");
+          break;
+        case 6:
+          button.attribute("title", "flip diagonal northwest");
+          break;
+        case 7:
+          button.attribute("title", "flip diagonal northeast");
+          break;
+      }
+
+      let svg = new p5.Element(button.child()[0]);
+      let displayScale = parseInt(
+        new p5.Element(root).style("--display-scale").split("px")[0]
+      );
+      if (layout_array[i][j] == 0) {
+        svg.style("width", displayScale * 0.8 + "px");
+        svg.style("height", displayScale * 0.8 + "px");
+      } else if (layout_array[i][j] <= 3) {
+        svg.style("width", displayScale * 1.6 + "px");
+        svg.style("height", displayScale * 1.6 + "px");
+      } else if (layout_array[i][j] <= 5) {
+        svg.style("width", displayScale * 1.2 + "px");
+        svg.style("height", displayScale * 1.2 + "px");
+      } else {
+        svg.style("width", displayScale * 1.1 + "px");
+        svg.style("height", displayScale * 1.1 + "px");
+      }
+    }
+    if (i == 0) {
+      let wButtonDiv = createDiv();
+      wButtonDiv.parent(row);
+      wButtonDiv.class("layout-wh-container width");
+      createPlusMinusButton(true, wButtonDiv, -1);
+      createPlusMinusButton(true, wButtonDiv, 1);
+    }
+  }
+
+  let hButtonDiv = createDiv();
+  hButtonDiv.parent(layoutDiv);
+  hButtonDiv.class("layout-wh-container height");
+  createPlusMinusButton(false, hButtonDiv, -1);
+  createPlusMinusButton(false, hButtonDiv, 1);
+}
